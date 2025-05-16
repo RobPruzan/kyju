728b2d7596912a0e31f96a2dd73b2a76281d1504
RobPruzan <rapruzan@buffalo.edu>
2025-05-16 08:33:28 -0400

    api v27

--- src/api/iframe.ts
+++ src/api/iframe.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 let iframeAccRef = 0;
-window.iframeData(69);
+window.iframeData = 69;
 
 export const IFrameStore = kyju.createDependencies(null, {
   // all values here are accessible on client
